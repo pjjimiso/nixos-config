c0c71dfa953ceeba25906c2638adb6c2f7b4a7a4
Patrick Jimison <patrick.j.jimison@intel.com>
2026-02-24 10:34:11 -0700

    Separate wsl and legion (laptop) configs

---
+++ hosts/legion/configuration.nix
@@ -0,0 +1,40 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware.nix
+    ../../modules/common.nix
+  ];
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.networkmanager.enable = true;
+
+  # Cinnamon desktop environment
+  services.xserver.enable = true;
+  services.xserver.desktopManager.cinnamon.enable = true;
+  services.displayManager.lightdm.enable = true;
+
+  # Audio
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # User account
+  users.users.pjjimiso = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+  };
+
+  # This value should match the NixOS release used during installation.
+  # Check /etc/nixos/configuration.nix on the laptop if unsure.
+  system.stateVersion = "25.05";
+}
---
+++ hosts/legion/hardware.nix
@@ -0,0 +1,5 @@
+# Copy the contents of /etc/nixos/hardware-configuration.nix from your laptop here.
+# On the laptop, run:
+#   cat /etc/nixos/hardware-configuration.nix
+# Then replace this file with that content.
+{ ... }: { }
