be132583ccbe36000e2662fbd81cf0598bd5f2c9
Patrick Jimison <patrick.j.jimison@intel.com>
2026-02-25 08:46:48 -0700

    Add tmux, git, neovim, and tmuxinator to home-manager

--- home/default.nix
+++ home/default.nix
@@ -5,6 +5,18 @@
   home.homeDirectory = "/home/pjjimiso";
   home.stateVersion = "25.05";
 
+  # Clone oh-my-tmux if not already present
+  home.activation.cloneOhMyTmux = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    if [ ! -f "$HOME/.config/tmux/tmux.conf" ]; then
+      $DRY_RUN_CMD mkdir -p $HOME/.config/tmux
+      $DRY_RUN_CMD ${pkgs.git}/bin/git clone https://github.com/gpakosz/.tmux.git /tmp/oh-my-tmux
+      $DRY_RUN_CMD cp /tmp/oh-my-tmux/.tmux.conf $HOME/.config/tmux/tmux.conf
+      $DRY_RUN_CMD rm -rf /tmp/oh-my-tmux
+    fi
+  '';
+
+  home.file.".config/tmux/tmux.conf.local".source = ./tmux/tmux.conf.local;
+
   # Clone Neovim config if not already present
   home.activation.cloneNvimConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     if [ ! -d "$HOME/.config/nvim" ]; then
@@ -12,7 +24,11 @@
     fi
   '';
 
-  home.packages = [
+  home.packages = with pkgs; [
+    git
+    tmux
+    tmuxinator
+    neovim
     inputs.claude-code.packages.${pkgs.system}.default
   ];
 
