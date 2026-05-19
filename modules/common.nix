76c49305125ab1b642de0f1139a98e8d1f899e81
Patrick Jimison <pat.jimison@gmail.com>
2026-05-19 10:59:50 -0700

    Remove synchat dns routing

--- modules/common.nix
+++ modules/common.nix
@@ -17,7 +17,6 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   networking.hosts = {
-    "127.0.0.1" = [ "synchat.internal" "synchatapi.internal" ];
     "127.0.1.1" = [ "nixos.clients.intel.com" "nixos" ];
   };
 
