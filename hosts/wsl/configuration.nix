4f879eff0057b34c72d6426193e0b54790e5bf30
Patrick Jimison <pat.jimison@gmail.com>
2026-06-10 21:45:34 -0700

    Add win32yank windows install to PATH on wsl

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -7,16 +7,18 @@
   wsl.defaultUser = "pjjimiso";
   wsl.wslConf.network.generateHosts = false;
 
-  # nvim copy to clipboard
-  environment.systemPackages = with pkgs; [ win32yank ];
-
   users.users.pjjimiso.extraGroups = [ "docker" ];
 
   networking.proxy.default = "http://proxy-chain.intel.com:912";
   networking.proxy.noProxy = "127.0.0.1,localhost,intel.com";
 
   home-manager.extraSpecialArgs = { inherit inputs; corporate = true; };
-  home-manager.users.pjjimiso = import ../../home/default.nix;
+  home-manager.users.pjjimiso = { 
+    imports = [ ../../home/default.nix ];
+    home.sessionPath = [
+      "/mnt/c/win32yank/"
+    ];
+  };
 
   system.stateVersion = "25.05";
 }
