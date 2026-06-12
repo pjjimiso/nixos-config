2b253aaa96870087af9320fb71e87046e5014636
Patrick Jimison <pat.jimison@gmail.com>
2026-06-12 16:13:06 -0700

    tmux-ssh-finder shows the user, proxyjump server, and identityfile it will use for ssh connection

--- home/tmux/tmux-ssh-finder.sh
+++ home/tmux/tmux-ssh-finder.sh
@@ -6,7 +6,11 @@ CONFIG="${HOME}/.ssh/config"
 
 host=$(awk '
 /^[[:space:]]*[Hh]ost[[:space:]]/ && $2 !~ /[*?!]/ { print $2 }
-' "$CONFIG" | sort -u | fzf --prompt="ssh Γ¥» " --reverse --height=100%) || exit 0
+' "$CONFIG" | sort -u | fzf \
+    --prompt="ssh >> " --reverse --height=100% \
+    --preview='ssh -G {} 2>/dev/null | awk "/^(user|hostname|port|identityfile|proxyjump|proxycommand) /" | sort' \
+    --preview-window='right:50%:wrap') || exit 0
+
 
 [ -z "$host" ] && exit 0
 
