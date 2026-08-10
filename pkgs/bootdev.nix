a3ed65058b2beeffd2a673807f1920ec8829c588
Patrick Jimison <pat.jimison@gmail.com>
2026-08-10 11:33:03 -0700

    Update bootdev cli, add special handling of sleep call in bootdev test case to make it work in nix build sandbox

--- pkgs/bootdev.nix
+++ pkgs/bootdev.nix
@@ -1,4 +1,4 @@
-{ lib, buildGoModule, src }:
+{ lib, buildGoModule, coreutils, src }:
 
 buildGoModule {
   pname = "bootdev";
@@ -6,6 +6,14 @@ buildGoModule {
   inherit src;
 
   vendorHash = "sha256-ZDioEU5uPCkd+kC83cLlpgzyOsnpj2S7N+lQgsQb8uY=";
+
+  # The timeout test spawns a fake `go` that execs /bin/sleep, which the nix build
+  # sandbox does not provide (only /bin/sh exists)
+  # Replace /bin/sleep with nix store's ${coreutils}/bin/sleep to make it work in the sandbox
+  postPatch = ''
+    substituteInPlace version/version_test.go \
+      --replace-fail "exec /bin/sleep 5" "exec ${coreutils}/bin/sleep 5"
+  '';
 
   meta = {
     description = "The official Boot.dev CLI client";
